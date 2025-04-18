38289214114addf304dfc47ac2e579907ae67f75
Jeremiah <focustoexpand@gmail.com>
2025-04-17 19:05:26 -0500

    completed all, was very difficult

--- script.js
+++ script.js
@@ -16,5 +16,51 @@ function getHumanChoice() {
     return userChoice
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+  
+    function playRound(humanChoice, computerChoice) {
+      console.log("Human chose:", humanChoice);
+      console.log("Computer chose:", computerChoice);
+  
+      humanChoice = humanChoice.toLowerCase();
+  
+      if (humanChoice === computerChoice) {
+        console.log("It's a tie!");
+      } else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+      ) {
+        console.log("You win!");
+        humanScore++;
+      } else {
+        console.log("You lose jackass!");
+        computerScore++;
+      }
+  
+      console.log("Current Scores - Humans:", humanScore, "| Computer:", computerScore);
+    }
+    }
+  
+    // Play 5 rounds
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+  
+    console.log("Final Scores - Human:", humanScore, "| Computer:", computerScore);
+  
+    if (humanScore > computerScore) {
+      console.log("You win jackass!");
+    } else if (humanScore < computerScore) {
+      console.log("You lose dumbass!");
+    } else {
+      console.log("Dumbass you tied!");
+    }
+  
+  playGame();
+  
